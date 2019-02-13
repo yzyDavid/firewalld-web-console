70fdb69434a3e53cf528e4d2aa1f1c2f19649065
Zhenyun Yu <me@yuzhenyun.me>
2019-02-13 18:56:16 +0800

    added hello-world agent.

--- agent/main.go
+++ agent/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net"
+	
+	grpc "google.golang.org/grpc"
+	service "github.com/yzyDavid/firewalld-web-console/proto"
+)
 
+const (
+	port = ":32116"
+)
+
+var _ service.StatusResponse
+
+type server struct {}
+
+func (s *server) GetStatus(ctx context.Context, in *service.StatusRequest) (*service.StatusResponse, error) {
+	return &service.StatusResponse{}, nil
+}
+
+func main() {
+	log.Print("agent started.")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Printf("listening %s", port)
+	s := grpc.NewServer()
+	service.RegisterAgentServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+	        log.Fatalf("failed to serve: %v", err)
+	}
 }
